ca5b185ed44f659568f8d10794e1595049f724a4
James King <metapyziks@gmail.com>
2013-11-03 12:50:52 +0000

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+$('a[href^=#]').on("click",function(e){
+    var t= $(this.hash);
+    var t=t.length&&t||$('[name='+this.hash.slice(1)+']');
+    if(t.length){
+        var tOffset=t.offset().top;
+        $('html,body').animate({scrollTop:tOffset-20},'slow');
+        e.preventDefault();
+    }
+});
