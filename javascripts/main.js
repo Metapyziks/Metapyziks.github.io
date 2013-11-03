557962a89a195b1278b3a516c8d3652078aa4858
James King <metapyziks@gmail.com>
2013-11-03 13:02:30 +0000

    Fixed images and added easing

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,11 @@
-$('a[href^=#]').on("click",function(e){
-    var t= $(this.hash);
-    var t=t.length&&t||$('[name='+this.hash.slice(1)+']');
-    if(t.length){
-        var tOffset=t.offset().top;
-        $('html,body').animate({scrollTop:tOffset-20},'slow');
-        e.preventDefault();
-    }
+$(function() {
+    $('a[href^=#]').on("click",function(e){
+        var t= $(this.hash);
+        var t=t.length&&t||$('[name='+this.hash.slice(1)+']');
+        if(t.length){
+            var tOffset=t.offset().top;
+            $('html,body').animate({scrollTop:tOffset-20},'slow', 'swing');
+            e.preventDefault();
+        }
+    });
 });
